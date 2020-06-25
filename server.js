8390dd8f26bcc5983626c4a4bc9d0ee6837dd159
Frederica Blissett aka Samri <redricasa@users.noreply.github.com>
2020-06-24 21:30:40 -0700

    added materialize input with the icons

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 require('./models/db');
 
 const express = require('express');
-
+const path = require('path');
+const exphbs= require('express-handlebars');
 const employeeController = require('./controllers/employeeController');
 
 var app = express();
+
+app.set('Views', path.join(__dirname, '/Views/'));
+app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/Views/Layouts/' }));
+app.set('view engine', 'hbs');
 // application server(gate keeper) listening on an open port 3000
 app.listen(3000, () => {
     console.log('Express server started at port : 3000')
