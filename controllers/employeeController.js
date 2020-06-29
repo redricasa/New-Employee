09b063d24639b9ddbbd807b8b03ebbe14ff24806
Frederica Blissett aka Samri <redricasa@users.noreply.github.com>
2020-06-29 13:50:30 -0700

    added GET method to addoredit.hbs and console.log to post method in controller

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -5,35 +5,16 @@ var util = require('util');
 const empModel = require('../models/employee.model');
 console.log("EmployeeModel", empModel);
 var router = express.Router();
-router.post('/', (req, res)=>{
-    // var newEmpl= empModel(
-    //     {"fullname": "name",
-    //     "email": "emailfkajsdlkfj",
-    //     "mobile": 349850293845, 
-    //     "city": "alksdjflak"
-    // });
-    // newEmpl.save();
-    res.send();
-});
-// fetch employees from database
-// router.get( '/', (req, res) => {
-    // get all documents
-    // var cursor= empModel.find({});
-    // traversed through cursor object to get the schema
-    // console.log("_____", cursor.schema.obj);
-    // TODO: get the res to send documents from EmployeeDB collection to localhost:3000/employee
-    // res.send(cursor);
-    // asynchronous function
-    // cursor.then (function(documents){
-    //     console.log("DOCUMENTS", documents);
-    //     res.send(documents);
-    //     console.log("DOCUMENTS", documents);
-    // });   
-// });
+
+
 router.get('/', function(req, res) {
     // render addOrEdit.hbs
     res.render("employee/addOrEdit", {
         viewTitle: "Insert Employee"
     })
-});  
+}); 
+
+router.post('/', (req, res)=>{
+    console.log("hello")
+});
 module.exports = router; 
